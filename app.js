885321a3e70cdc3c35d5105004de931a6e1e8b35
luca-conti <lucagopro12@gmail.com>
2025-02-06 08:35:53 +1300

    js en

--- app.js
+++ app.js
@@ -25,31 +25,28 @@ function verificadorDeNumeroPalindro() {
 
 
 function verificadorDeNumeroPalindroIT() {
-    function verificadorDeNumeroPalindro() {
-        var numero = document.getElementById('numero').value;
-            var numeroPalindro = numero.split("");
-            console.log(numeroPalindro);
-            var numeroInvertido = numeroPalindro.reverse();
-            console.log(numeroInvertido);
-            numeroInvertido = numeroInvertido.join("");
-            console.log(numeroInvertido);
-            var exibir = document.getElementById('exibir');
-    
-            if (numero == numeroInvertido) {
-            let tira = document.querySelector('h1')
-            tira.innerHTML = ''
-                exibir.innerHTML = `Il numero ${numero} è un palindro.`;
-                console.log(1);
-            } else {
-                exibir.innerHTML = `Il numero ${numero} non è un palindro.`;
-                console.log(2);
-            };
-    }
+    var numero = document.getElementById('numero').value;
+    var numeroPalindro = numero.split("");
+    console.log(numeroPalindro);
+    var numeroInvertido = numeroPalindro.reverse();
+    console.log(numeroInvertido);
+    numeroInvertido = numeroInvertido.join("");
+    console.log(numeroInvertido);
+    var exibir = document.getElementById('exibir');
 
+    if (numero == numeroInvertido) {
+    let tira = document.querySelector('h1')
+    tira.innerHTML = ''
+        exibir.innerHTML = ``;
+        console.log(1);
+    } else {
+        exibir.innerHTML = ``;
+        console.log(2);
+    };
 }
 
+
 function verificadorDeNumeroPalindroen() {
-    function verificadorDeNumeroPalindro() {
         var numero = document.getElementById('numero').value;
             var numeroPalindro = numero.split("");
             console.log(numeroPalindro);
@@ -68,12 +65,24 @@ function verificadorDeNumeroPalindroen() {
                 exibir.innerHTML = `The number ${number} is not a palinder.`;
                 console.log(2);
             };
-    }
-}
+    };
 
 function reiniciar() {
     document.getElementById('numero').value = '';
     document.getElementById('exibir').innerHTML = '';
     document.querySelector('h1').textContent = 'Digite um numero, que eu falarei se é um palindro ou não.'
 
+}
+
+function reiniciaren() {
+    document.getElementById('numero').value = '';
+    document.getElementById('exibir').innerHTML = '';
+    document.querySelector('h1').textContent = `Enter a number, and I'll tell you whether it's a palindro or not.`
+
+}
+
+function reiniciarit() {
+    document.getElementById('numero').value = '';
+    document.getElementById('exibir').innerHTML = '';
+    document.querySelector('h1').textContent = `Comporre un numero, che indichi se si tratta di un palindro o meno.`
 }
